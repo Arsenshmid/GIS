1b24537534601fc8dc25c4698447c0a77bb84404
Arsenshmid <apce2@mail.ru>
2024-04-06 16:58:35 +0900

    commit

--- mysite/сгенерировать тестовые данные.py
+++ mysite/сгенерировать тестовые данные.py
@@ -5,19 +5,19 @@ import numpy as np
 df = pd.DataFrame(columns=['latitude', 'longitude', 'weight', 'irrigated', 'crop'])
 
 # Задаем границы области внутри Якутска (примерно 38x38 метров)
-min_lat, max_lat = 62.011, 62.0115  # Уменьшаем границы
-min_lon, max_lon = 129.011, 129.0115  # Уменьшаем границы
+min_lat, max_lat = 62.011, 62.0118  # Уменьшаем границы
+min_lon, max_lon = 129.011, 129.0118  # Уменьшаем границы
 
 # Задаем шаг для генерации точек
 step = 0.00005  # Увеличиваем шаг до примерно 50 метров
 
 # Заполняем DataFrame данными
-prev_weight = np.random.randint(0, 1001)  # вес урожая в кг (от 0 до 1000 кг)
+prev_weight = np.random.randint(0, 101)  # вес урожая в кг (от 0 до 1000 кг)
 for lat in np.arange(min_lat, max_lat, step):
     for lon in np.arange(min_lon, max_lon, step):
         weight = prev_weight + np.random.randint(-50, 51)  # вес урожая в кг (от 0 до 1000 кг)
         weight = max(0, weight)  # убедимся, что вес не станет отрицательным
-        weight = min(1000, weight)  # убедимся, что вес не превысит 1000
+        weight = min(100, weight)  # убедимся, что вес не превысит 1000
         prev_weight = weight
         irrigated = np.random.choice([True, False])  # орошалась ли область
         crop = np.random.choice(['wheat', 'corn', 'soy'])  # какая культура была посажена
